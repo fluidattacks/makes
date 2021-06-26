d2db87ee36d2856204a3b30eb5744376d2cb5c9b
Kevin Amado <kamadorueda@gmail.com>
2021-06-26 02:23:20 -0500

    feat(cross): module system

--- default.nix
+++ default.nix
@@ -1,11 +1,42 @@
-let
-  sources = import ./nix/sources.nix;
+rec {
+  makes = {
+    # Makes CLI, used for orchestrating the whole thing
+    #   Dev with: $(nix-build -A makes.m)/bin/m
+    #   Install with: nix-env --install -A makes.m -f .
+    #   Run with: m
+    m = packages.nixpkgs.writeShellScriptBin "m" ''
+      _NIX_INSTANTIATE=${packages.nixpkgs.nix}/bin/nix-instantiate \
+      LD_LIBRARY_PATH=/not-set \
+      PATH=/not-set \
+      PYTHONPATH=${makes.src} \
+      ${packages.nixpkgs.python3.withPackages (_: [
+        packages.nixpkgs.python3Packages.click
+      ])}/bin/python -m cli "$@"
+    '';
+
+    # Makes module system evaluator
+    module =
+      { head # Path to the user's project
+      , ...
+      }:
+      let
+        args = {
+          path = path: head + path;
+        };
+      in
+      packages.nixpkgs.lib.modules.evalModules {
+        modules = [
+          (makes.src)
+          (args.path "/makes.nix")
+        ];
+        specialArgs = args;
+      };
+
+    # Makes' implementation source code
+    src = ./src;
+  };
   packages = {
     nixpkgs = import sources.nixpkgs { };
   };
-in
-{
-  m = packages.nixpkgs.writeShellScriptBin "m" ''
-    ${packages.nixpkgs.python3}/bin/python ${./src/cli.py} "$@"
-  '';
+  sources = import ./nix/sources.nix;
 }
