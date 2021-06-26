91ccddf0317b9edba24f60a6dd1295c3573c76bb
Kevin Amado <kamadorueda@gmail.com>
2021-06-26 18:38:37 -0500

    feat(build): support make template

---
+++ src/args/make-template/default.nix
@@ -0,0 +1,51 @@
+{ __packages
+, builtinLambdas
+, builtinShellCommands
+, builtinShellOptions
+, makeDerivation
+, makeSearchPaths
+, path
+, ...
+}:
+
+{ arguments ? { }
+, argumentsBase64 ? { }
+, name
+, searchPaths ? { }
+, template ? ""
+}:
+let
+  # Validate arguments
+  validateArguments = builtins.mapAttrs
+    (k: v: (
+      if __packages.nixpkgs.lib.strings.hasPrefix "env" k
+      then v
+      else abort "Ivalid argument: ${k}, arguments must start with `env`"
+    ));
+
+  arguments' = validateArguments arguments;
+  argumentsBase64' = validateArguments argumentsBase64;
+in
+makeDerivation {
+  arguments = arguments' // argumentsBase64' // {
+    __envArgumentNamesFile = builtinLambdas.listToFileWithTrailinNewLine
+      (builtins.attrNames arguments);
+    __envArgumentBase64NamesFile = builtinLambdas.listToFileWithTrailinNewLine
+      (builtins.attrNames argumentsBase64);
+    __envPath = __packages.nixpkgs.lib.strings.makeBinPath [
+      __packages.nixpkgs.gnugrep
+      __packages.nixpkgs.gnused
+    ];
+    __envTemplate =
+      if searchPaths == { }
+      then builtinLambdas.asContent template
+      else ''
+        source "${makeSearchPaths searchPaths}"
+
+        ${builtinLambdas.asContent template}
+      '';
+  };
+  builder = ../../../src/args/make-template/builder.sh;
+  local = true;
+  inherit name;
+}
