f82b088fe454223e892f921715b5d871a52db69f
Kevin Amado <kamadorueda@gmail.com>
2021-06-26 22:11:34 -0500

    feat(back): allow executing applications

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,13 @@
 import click
-import os
+import json
+from os import (
+    environ,
+    getcwd,
+)
+from os.path import (
+    exists,
+    join,
+)
 import subprocess
 from typing import (
     List,
@@ -15,23 +23,41 @@ def _if(condition: bool, value: str) -> List[str]:
 @click.command(help="A SecDevOps Framework powered by Nix.")
 @click.option("--debug", is_flag=True)
 @click.argument("attr")
+@click.argument("args", nargs=-1)
 def cli(
     attr: str,
+    args: List[str],
     debug: bool,
 ) -> None:
-    with subprocess.Popen(
+    cwd: str = getcwd()
+    out: str = join(cwd, f"result{attr.replace('.', '-')}")
+    actions_path: str = join(out, "makes-actions.json")
+
+    process: subprocess.CompletedProcess = subprocess.run(
         args=[
-            os.environ["_NIX_BUILD"],
+            environ["_NIX_BUILD"],
             "--arg",
             "head",
             "./.",
             "--attr",
             f"config.outputs{attr}",
+            "--out-link",
+            out,
             *_if(debug, "--show-trace"),
-            os.environ["_EVALUATOR"],
+            environ["_EVALUATOR"],
         ],
-    ) as _:
-        pass
+        check=False,
+    )
+
+    if process.returncode == 0:
+        if exists(actions_path):
+            with open(actions_path) as actions_file:
+                for action in json.load(actions_file):
+                    if action["type"] == "exec":
+                        subprocess.run(
+                            args=[join(out, action["location"][1:]), *args],
+                            check=False,
+                        )
 
 
 if __name__ == "__main__":
