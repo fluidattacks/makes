e751593475101dd9f6cae7c8ac64992fe005a1a7
Kevin Amado <kamadorueda@gmail.com>
2021-06-28 17:01:08 -0500

    feat(build): builtin outputs module

---
+++ src/modules/outputs/builtins/hello-world.nix
@@ -0,0 +1,31 @@
+{ makeEntrypoint
+, ...
+}:
+{ config
+, lib
+, ...
+}:
+{
+  options = {
+    helloWorld = {
+      enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+      };
+      name = lib.mkOption {
+        type = lib.types.str;
+      };
+    };
+  };
+  config = {
+    outputs = {
+      helloWorld = lib.mkIf config.helloWorld.enable (makeEntrypoint {
+        name = "hello-world";
+        entrypoint = ''
+          info Hello from Makes! ${config.helloWorld.name}.
+          info You called us with CLI arguments: [ "$@" ].
+        '';
+      });
+    };
+  };
+}
