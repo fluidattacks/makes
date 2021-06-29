c9cf790132cbdbbc259d609e55c71a595a23e81a
Kevin Amado <kamadorueda@gmail.com>
2021-06-29 12:00:27 -0500

    feat(build): build makes with makes

--- makes/main.nix
+++ makes/main.nix
@@ -1,7 +1,26 @@
-{ makeDerivation
+{ makeEntrypoint
+, inputs
+, path
 , ...
 }:
-makeDerivation {
-  builder = "touch $out";
-  name = "test";
+makeEntrypoint {
+  arguments = {
+    envNix = inputs.makesPackages.nixpkgs.nix;
+    envSrc = path "/src";
+  };
+  entrypoint = ''
+    _EVALUATOR=__envSrc__/evaluator.nix \
+    _NIX_BUILD=__envNix__/bin/nix-build \
+    _NIX_INSTANTIATE=__envNix__/bin/nix-instantiate \
+    python -m cli "$@"
+  '';
+  searchPaths = {
+    envPaths = [
+      inputs.makesPackages.nixpkgs.python38
+    ];
+    envPythonPaths = [
+      (path "/src")
+    ];
+  };
+  name = "m";
 }
