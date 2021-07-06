b55e1844a905d12d3e83f737880f66a018d08484
Kevin Amado <kamadorueda@gmail.com>
2021-07-06 14:33:06 -0500

    feat(conf): release 21.08-pre1

--- makes.nix
+++ makes.nix
@@ -13,7 +13,7 @@
       makesGitHubMonthly = {
         src = config.outputs."container-image";
         registry = "ghcr.io";
-        tag = "fluidattacks/makes:$(date -d +1month +%y.%m)";
+        tag = "fluidattacks/makes:21.08-pre1";
       };
       makesGitLab = {
         src = config.outputs."container-image";
@@ -23,7 +23,7 @@
       makesGitLabMonthly = {
         src = config.outputs."container-image";
         registry = "registry.gitlab.com";
-        tag = "fluidattacks/product/makes:$(date -d +1month +%y.%m)";
+        tag = "fluidattacks/product/makes:21.08-pre1";
       };
     };
   };
