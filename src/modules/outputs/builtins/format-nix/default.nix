1a9d3717a038025577942daf6808b6197d3a0d1b
Daniel Salazar <dsalazar@fluidattacks.com>
2021-07-07 14:36:00 -0500

    feat(build): nix formatter
    - Add util

---
+++ src/modules/outputs/builtins/format-nix/default.nix
@@ -0,0 +1,42 @@
+{ builtinLambdas
+, inputs
+, lib
+, makeScript
+, ...
+}:
+{ config
+, ...
+}:
+{
+  options = {
+    formatNix = {
+      enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+      };
+      targets = lib.mkOption {
+        default = [ "/" ];
+        type = lib.types.listOf lib.types.str;
+      };
+    };
+  };
+  config = {
+    outputs = {
+      "/formatNix" = lib.mkIf config.formatNix.enable (makeScript {
+        arguments = {
+          envTargets = builtinLambdas.asBashArray
+            (builtins.map
+              (target: "./${target}")
+              config.formatNix.targets);
+        };
+        name = "format-nix";
+        searchPaths = {
+          envPaths = [
+            inputs.makesPackages.nixpkgs.nixpkgs-fmt
+          ];
+        };
+        entrypoint = ./entrypoint.sh;
+      });
+    };
+  };
+}
