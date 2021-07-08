a35bb17a78d975e10ec0368c197cca666d0b44e2
Daniel Salazar <dsalazar@fluidattacks.com>
2021-07-08 16:25:01 -0500

    feat(build): #158 markdown linter
    - Add util
    - Add to makes

---
+++ src/modules/outputs/builtins/lint-markdown/default.nix
@@ -0,0 +1,44 @@
+{ builtinLambdas
+, inputs
+, lib
+, makeDerivation
+, path
+, ...
+}:
+{ config
+, ...
+}:
+{
+  options = {
+    lintMarkdown = {
+      enable = lib.mkOption {
+        default = false;
+        type = lib.types.bool;
+      };
+      targets = lib.mkOption {
+        default = [ "/" ];
+        type = lib.types.listOf lib.types.str;
+      };
+    };
+  };
+  config = {
+    outputs = {
+      "/lintMarkdown" = lib.mkIf config.lintMarkdown.enable (makeDerivation {
+        arguments = {
+          envStyle = path /src/modules/outputs/builtins/lint-markdown/style.rb;
+          envTargets = builtinLambdas.asBashArray
+            (builtins.map
+              path
+              config.lintMarkdown.targets);
+        };
+        name = "lint-markdown";
+        searchPaths = {
+          envPaths = [
+            inputs.makesPackages.nixpkgs.mdl
+          ];
+        };
+        builder = ./builder.sh;
+      });
+    };
+  };
+}
