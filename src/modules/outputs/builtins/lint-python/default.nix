883b419b81c8bd1c163d13a8d6746eacc6d6358b
Kevin Amado <kamadorueda@gmail.com>
2021-07-08 10:42:27 -0500

    feat(build): lint python dir of modules
    - Gonna do it in parallel, pending to implement
    - Add options and base skeleton

--- src/modules/outputs/builtins/lint-python/default.nix
+++ src/modules/outputs/builtins/lint-python/default.nix
@@ -8,6 +8,79 @@
 { config
 , ...
 }:
+let
+  makeModule = name: { python, src }: {
+    name = "/lintPython/module/${name}";
+    value = makeDerivation {
+      arguments = {
+        envSettingsMypy = ./settings-mypy.cfg;
+        envSettingsProspector = ./settings-prospector.yaml;
+        envSrc = path src;
+      };
+      name = "lint-python-module-for-${name}";
+      searchPaths = {
+        envPaths = [
+          inputs.makesPackages.nixpkgs.findutils
+        ];
+        envSources = [
+          (makePythonEnvironment {
+            dependencies = [
+              "mypy==0.910"
+              "prospector==1.3.1"
+              "returns==0.16.0"
+            ];
+            name = "lint-python-for-${name}";
+            inherit python;
+            subDependencies = [
+              "astroid==2.4.1"
+              "dodgy==0.2.1"
+              "flake8-polyfill==1.0.2"
+              "flake8==3.9.2"
+              "isort==4.3.21"
+              "lazy-object-proxy==1.4.3"
+              "mccabe==0.6.1"
+              "mypy-extensions==0.4.3"
+              "pep8-naming==0.10.0"
+              "pycodestyle==2.6.0"
+              "pydocstyle==6.1.1"
+              "pyflakes==2.2.0"
+              "pylint-celery==0.3"
+              "pylint-django==2.1.0"
+              "pylint-flask==0.6"
+              "pylint-plugin-utils==0.6"
+              "pylint==2.5.3"
+              "PyYAML==5.4.1"
+              "requirements-detector==0.7"
+              "setoptconf==0.2.0"
+              "six==1.16.0"
+              "snowballstemmer==2.1.0"
+              "toml==0.10.2"
+              "typing-extensions==3.10.0.0"
+              "wrapt==1.12.1"
+            ];
+          })
+        ];
+      };
+      builder = ./builder-module.sh;
+    };
+  };
+  makeDirOfModules = name: { src, ... }:
+    let
+      moduleNames = builtins.attrNames (builtins.readDir (path src));
+      moduleSrcs = builtins.map (name: path "${src}/${name}") moduleNames;
+    in
+    {
+      name = "/lintPython/dirOfModules/${name}";
+      value = makeDerivation {
+        arguments = {
+          envModules = moduleSrcs;
+        };
+        name = "lint-python-dir-of-modules-for-${name}";
+        searchPaths = { };
+        builder = ./builder-dir-of-modules.sh;
+      };
+    };
+in
 {
   options = {
     lintPython = {
@@ -15,7 +88,21 @@
         default = false;
         type = lib.types.bool;
       };
-      packages = lib.mkOption {
+      dirsOfModules = lib.mkOption {
+        default = { };
+        type = lib.types.attrsOf (lib.types.submodule (_: {
+          options = {
+            python = lib.mkOption {
+              type = lib.types.enum [ "3.7" "3.8" "3.9" ];
+            };
+            src = lib.mkOption {
+              type = lib.types.str;
+            };
+          };
+        }));
+      };
+      modules = lib.mkOption {
+        default = { };
         type = lib.types.attrsOf (lib.types.submodule (_: {
           options = {
             python = lib.mkOption {
@@ -31,62 +118,11 @@
   };
   config = {
     outputs = lib.mkIf config.lintPython.enable
+      ((lib.attrsets.mapAttrs'
+        makeModule
+        config.lintPython.modules) //
       (lib.attrsets.mapAttrs'
-        (name: { python, src }: {
-          name = "/lintPython/${name}";
-          value = makeDerivation {
-            arguments = {
-              envSettingsMypy = ./settings-mypy.cfg;
-              envSettingsProspector = ./settings-prospector.yaml;
-              envSrc = path src;
-            };
-            name = "lint-python-for-${name}";
-            searchPaths = {
-              envPaths = [
-                inputs.makesPackages.nixpkgs.findutils
-              ];
-              envSources = [
-                (makePythonEnvironment {
-                  dependencies = [
-                    "mypy==0.910"
-                    "prospector==1.3.1"
-                    "returns==0.16.0"
-                  ];
-                  name = "lint-python-for-${name}";
-                  inherit python;
-                  subDependencies = [
-                    "astroid==2.4.1"
-                    "dodgy==0.2.1"
-                    "flake8-polyfill==1.0.2"
-                    "flake8==3.9.2"
-                    "isort==4.3.21"
-                    "lazy-object-proxy==1.4.3"
-                    "mccabe==0.6.1"
-                    "mypy-extensions==0.4.3"
-                    "pep8-naming==0.10.0"
-                    "pycodestyle==2.6.0"
-                    "pydocstyle==6.1.1"
-                    "pyflakes==2.2.0"
-                    "pylint-celery==0.3"
-                    "pylint-django==2.1.0"
-                    "pylint-flask==0.6"
-                    "pylint-plugin-utils==0.6"
-                    "pylint==2.5.3"
-                    "PyYAML==5.4.1"
-                    "requirements-detector==0.7"
-                    "setoptconf==0.2.0"
-                    "six==1.16.0"
-                    "snowballstemmer==2.1.0"
-                    "toml==0.10.2"
-                    "typing-extensions==3.10.0.0"
-                    "wrapt==1.12.1"
-                  ];
-                })
-              ];
-            };
-            builder = ./builder.sh;
-          };
-        })
-        config.lintPython.packages);
+        makeDirOfModules
+        config.lintPython.dirsOfModules));
   };
 }
