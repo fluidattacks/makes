3d1d5287f86f2db411a00bd23b093f0f9bd54738
Kevin Amado <kamadorueda@gmail.com>
2021-07-19 16:58:42 -0500

    refac(build): #252 split terraform secrets
    - Split in two files as they are now two
      different modules in the config

--- src/evaluator/modules/secrets-for-aws-from-env.nix
+++ src/evaluator/modules/secrets-for-aws-from-env.nix
@@ -1,6 +1,5 @@
 { __toModuleOutputs__
 , makeSecretForAwsFromEnv
-, makeSecretForTerraformFromEnv
 , ...
 }:
 { config
@@ -43,24 +42,12 @@ let
         inherit sessionToken;
       };
     };
-
-  makeSecretForTerraformFromEnvOutput = name: mapping: {
-    name = "/secretsForTerraformFromEnv/${name}";
-    value = makeSecretForTerraformFromEnv {
-      inherit name;
-      inherit mapping;
-    };
-  };
 in
 {
   options = {
     secretsForAwsFromEnv = lib.mkOption {
       default = { };
       type = lib.types.attrsOf secretForAwsFromEnvType;
-    };
-    secretsForTerraformFromEnv = lib.mkOption {
-      default = { };
-      type = lib.types.attrsOf (lib.types.attrsOf lib.types.str);
     };
   };
   config = {
@@ -77,9 +64,6 @@ in
             secretAccessKey = "AWS_SECRET_ACCESS_KEY";
             sessionToken = "AWS_SESSION_TOKEN";
           };
-        }) //
-      (__toModuleOutputs__
-        makeSecretForTerraformFromEnvOutput
-        config.secretsForTerraformFromEnv);
+        });
   };
 }
