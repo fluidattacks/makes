6fea9ceec6b78037d8f81334e6fe2d763fa7d5bb
Kevin Amado <kamadorueda@gmail.com>
2021-07-19 19:16:42 -0500

    feat(front): #252 display more info
    - Display what are we doing with secrets, it looks
      just awesome
    - I am uploading a showcase next so people get to
      know what makes is about in a page

--- src/args/make-env-vars/template.sh
+++ src/args/make-env-vars/template.sh
@@ -2,10 +2,13 @@
 
 function main {
   source __argMap__/makes-setup.sh map
-  for var in "${!map[@]}"; do
-    export "${var}=${map[${var}]}" \
-      || return 1
-  done
+
+  info Making environment variables for __argName__: \
+    && for var in "${!map[@]}"; do
+      info - "${var}=${map[${var}]}" \
+        && export "${var}=${map[${var}]}" \
+        || return 1
+    done
 }
 
 main "${@}"
--- src/args/make-secret-for-terraform-from-env/template.sh
+++ src/args/make-secret-for-terraform-from-env/template.sh
@@ -2,11 +2,14 @@
 
 function main {
   source __argMap__/makes-setup.sh map
-  for var in "${!map[@]}"; do
-    require_env_var "${map[${var}]}" \
-      && export "TF_VAR_${var}=${!map[${var}]}" \
-      || return 1
-  done
+
+  info Making secret for Terraform from environment variables for __argName__: \
+    && for var in "${!map[@]}"; do
+      require_env_var "${map[${var}]}" \
+        && info - "TF_VAR_${var}" \
+        && export "TF_VAR_${var}=${!map[${var}]}" \
+        || return 1
+    done
 }
 
 main "${@}"
