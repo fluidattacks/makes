8b8fa7ebde4c64666203167c6458a159b5244acd
Kevin Amado <kamadorueda@gmail.com>
2021-08-02 21:39:27 -0500

    feat(build): #180 fail on diff
    - Fail on differences, so one can use this
      job to validate the gitlab-ci

--- src/evaluator/modules/pipelines/entrypoint-for-pipeline-on-gitlab.sh
+++ src/evaluator/modules/pipelines/entrypoint-for-pipeline-on-gitlab.sh
@@ -1,9 +1,22 @@
 # shellcheck shell=bash
 
+function is_up_to_date {
+  if test -e __argGitlabPath__; then
+    git --no-pager diff --no-index __argGitlabPath__ __argGitlabCiYaml__
+  else
+    git --no-pager diff --no-index /dev/null __argGitlabPath__
+  fi
+}
+
 function main {
+  local up_to_date
+
   info Generating gitlab-ci.yaml @ __argGitlabPath__ \
+    && if is_up_to_date; then up_to_date=true; else up_to_date=false; fi \
     && copy __argGitlabCiYaml__ __argGitlabPath__ \
-    && cat __argGitlabPath__
+    && if test "${up_to_date}" != true; then
+      error __argGitlabPath__ was not up to date
+    fi
 }
 
 main "${@}"
