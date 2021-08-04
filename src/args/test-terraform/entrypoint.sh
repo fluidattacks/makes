07e2c99feb70baa0cec910c01111a53d4e47d595
Kevin Amado <kamadorueda@gmail.com>
2021-08-04 09:16:03 -0500

    feat(build): #408 terraform debug
    - Support debugging in testTerraform

--- src/args/test-terraform/entrypoint.sh
+++ src/args/test-terraform/entrypoint.sh
@@ -1,12 +1,24 @@
 # shellcheck shell=bash
 
 function main {
+  local args=(
+    -lock=false
+    -refresh=true
+  )
+  export TF_LOG
+
   cd "$(mktemp -d)" \
     && copy '__argSrc__' . \
     && info Initializing '__argSrc__' \
     && terraform init \
     && info Testing '__argSrc__' \
-    && terraform plan -lock=false -refresh=true
+    && if test -n '__argDebug__'; then
+      TF_LOG=TRACE \
+        && args+=(
+          -parallelism=1
+        )
+    fi \
+    && terraform plan "${args[@]}"
 }
 
 main "${@}"
