376285a11b3100c77b209e6a9e659eb22d83e1ac
Kevin Amado <kamadorueda@gmail.com>
2021-08-19 14:03:40 -0500

    perf(build): #434 check versions faster
    - Check versions completeness before downloading

--- src/args/make-python-pypi-mirror/builder.sh
+++ src/args/make-python-pypi-mirror/builder.sh
@@ -7,7 +7,13 @@ function main {
     && copy "${envPyprojectToml}" pyproject.toml \
     && poetry env use "$(command -v python)" \
     && poetry lock \
-    && yj -tj < poetry.lock | yq -er '.metadata.files[][]|.file' > files.lst \
+    && yj -tj \
+      < poetry.lock > poetry.lock.json \
+    && jq -er '.package[]|"\(.name)==\(.version)"' \
+      < poetry.lock.json > closure.lst \
+    && jq -er '.metadata.files[][]|.file' \
+      < poetry.lock.json > files.lst \
+    && python "${envCheckCompleteness}" closure.lst "${envDeps}" "${envSubDeps}" \
     && mapfile -t files < files.lst \
     && mkdir downloads \
     && for file in "${files[@]}"; do
@@ -21,9 +27,7 @@ function main {
       --download-dir downloads \
       --mirror-dir "${out}/mirror" \
     && copy "${envDeps}" "${out}/deps.lst" \
-    && copy "${envSubDeps}" "${out}/sub-deps.lst" \
-    && python "${envCheckCompleteness}" \
-      "${out}/mirror" "${envDeps}" "${envSubDeps}"
+    && copy "${envSubDeps}" "${out}/sub-deps.lst"
 }
 
 function get_url {
