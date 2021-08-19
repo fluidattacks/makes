4732cf13b129397581bf4c7fe600bdf898115528
Kevin Amado <kamadorueda@gmail.com>
2021-08-19 18:16:12 -0500

    refac(build): #434 bootstrap pkgs
    - Wheel and setuptools are required to be boostraped
    - I have found a more elegant way than this but
      anyway I am flushing local code to avoid
      inventory

--- src/args/make-python-pypi-mirror/default.nix
+++ src/args/make-python-pypi-mirror/default.nix
@@ -1,7 +1,10 @@
 { __nixpkgs__
 , attrsMapToList
 , fakeSha256
+, fetchUrl
+, listOptional
 , makeDerivation
+, toBashArray
 , toFileLst
 , toFileToml
 , ...
@@ -11,6 +14,8 @@
 , python
 , sha256 ? fakeSha256
 , subDependencies ? { }
+, withSetupTools57 ? true
+, withWheel37 ? true
 }:
 let
   toReqsTxt = dependencies:
@@ -33,6 +38,16 @@ makeDerivation {
     envCheckCompleteness = ./check-completeness.py;
     envDeps = toReqsTxt dependencies;
     envSubDeps = toReqsTxt subDependencies;
+    envExtraFiles = toBashArray (builtins.concatLists [
+      (listOptional withSetupTools57 (fetchUrl {
+        url = "https://pypi.org/packages/py3/s/setuptools/setuptools-57.4.0-py3-none-any.whl";
+        sha256 = "1mhq6jw21sglccqmimydqi2rjvh3g5xjykb16gcvkkx6gabk14m4";
+      }))
+      (listOptional withWheel37 (fetchUrl {
+        url = "https://pypi.org/packages/py2.py3/w/wheel/wheel-0.37.0-py2.py3-none-any.whl";
+        sha256 = "1za6c4s0yjy1dzprmib3kph40hr8xgj3apdsnqs00v9wv4mln091";
+      }))
+    ]);
   };
   builder = ./builder.sh;
   name = "make-python-pypi-mirror-for-${name}";
