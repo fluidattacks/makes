ebd5447429032832177f5a828efccec8f8e5261b
Kevin Amado <kamadorueda@gmail.com>
2021-08-19 12:27:30 -0500

    feat(build): #434 mirror pypi
    - Mirror pypi by copying artifacts for the specific
      version
    - Use poetry for resolving sub-dependencies and a small
      script for the checking dependency completeness

--- src/args/make-python-pypi-mirror/default.nix
+++ src/args/make-python-pypi-mirror/default.nix
@@ -2,9 +2,8 @@
 , attrsMapToList
 , fakeSha256
 , makeDerivation
-, makePythonEnvironment
-, makePythonVersion
 , toFileLst
+, toFileToml
 , ...
 }:
 { dependencies ? { }
@@ -17,17 +16,20 @@ let
   toReqsTxt = dependencies:
     toFileLst "requirements.txt"
       (attrsMapToList (req: version: "${req}==${version}") dependencies);
-
-  pythonPypiMirror = makePythonEnvironment {
-    name = "python-pypi-mirror";
-    dependencies = [
-      "python-pypi-mirror==4.0.6"
-    ];
-    python = makePythonVersion "3.8";
-  };
 in
 makeDerivation {
   env = {
+    envPyprojectToml = toFileToml "pyproject.toml" {
+      build-system.build-backend = "poetry.core.masonry.api";
+      build-system.requires = [ "poetry-core>=1.0.0" ];
+      tool.poetry.authors = [ ];
+      tool.poetry.dependencies = dependencies // subDependencies // {
+        python = python.version;
+      };
+      tool.poetry.description = "";
+      tool.poetry.name = name;
+      tool.poetry.version = "1";
+    };
     envCheckCompleteness = ./check-completeness.py;
     envDeps = toReqsTxt dependencies;
     envSubDeps = toReqsTxt subDependencies;
@@ -35,8 +37,12 @@ makeDerivation {
   builder = ./builder.sh;
   name = "make-python-pypi-mirror-for-${name}";
   inherit sha256;
-  searchPaths = {
-    bin = [ __nixpkgs__.findutils python ];
-    source = [ pythonPypiMirror ];
-  };
+  searchPaths.bin = [
+    __nixpkgs__.curl
+    __nixpkgs__.poetry
+    __nixpkgs__.pypi-mirror
+    __nixpkgs__.yj
+    __nixpkgs__.yq
+    python
+  ];
 }
