1427e9be921c5c149dbdea3bcec829c440fbb00e
Kevin Amado <kamadorueda@gmail.com>
2021-08-20 18:34:44 -0500

    fix(build): #451 calculate cvss 3
    - Add a regression test

---
+++ makes/tests/calculateCvss3/main.nix
@@ -0,0 +1,9 @@
+# /path/to/my/project/makes/example/main.nix
+{ calculateCvss3
+, ...
+}:
+calculateCvss3
+  (builtins.concatStringsSep "" [
+    "CVSS:3.0/S:C/C:H/I:H/A:N/AV:P/AC:H/PR:H/UI:R/E:H/RL:O/RC:R/CR:H"
+    "/IR:X/AR:X/MAC:H/MPR:X/MUI:X/MC:L/MA:X"
+  ])
--- src/args/calculate-cvss-3/default.nix
+++ src/args/calculate-cvss-3/default.nix
@@ -14,10 +14,8 @@ makeDerivation {
   searchPaths = {
     source = [
       (makePythonPypiEnvironment {
-        dependencies.cvss = "2.3";
         name = "cvss";
-        python = makePythonVersion "3.8";
-        sha256 = "0n5ix3pbp8s88xjjgp4d8j1had8fcgymvgfj597x6rmrynqfqkq6";
+        sourcesJson = ./sources.json;
       })
     ];
   };
