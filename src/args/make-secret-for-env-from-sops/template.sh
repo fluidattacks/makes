b46ed6cebf86017d8e1546738ea2b405e169ff6d
Daniel Salazar <dsalazar@fluidattacks.com>
2021-08-23 10:38:49 -0500

    fix(back): #464 disable xpg echo
    - Completely disable shopt xpg_echo

--- src/args/make-secret-for-env-from-sops/template.sh
+++ src/args/make-secret-for-env-from-sops/template.sh
@@ -1,18 +1,15 @@
 # shellcheck shell=bash
 
-# This function temporarily disables xpg_echo as it breaks sops escaping
 function main {
   source __argVars__/template local vars
 
   info Making secrets for env from sops for __argName__: \
-    && shopt -u xpg_echo \
     && decrypted="$(sops --decrypt --output-type json '__argManifest__')" \
     && for var in "${vars[@]}"; do
       info - "${var}" \
         && export "${var//./__}=$(echo "${decrypted}" | jq -erc ".${var}")" \
         || return 1
-    done \
-    && shopt -s xpg_echo
+    done
 }
 
 main "${@}"
