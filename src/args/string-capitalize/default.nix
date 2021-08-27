6c49570e3fa7b10d261b3bc31d1874fb030f0597
Daniel Salazar <dsalazar@fluidattacks.com>
2021-08-27 11:22:07 -0500

    feat(back): #479 string capitalize arg
    - Add stringCapitalize arg

---
+++ src/args/string-capitalize/default.nix
@@ -0,0 +1,12 @@
+{ __nixpkgs__
+, ...
+}:
+str:
+let
+  lib = __nixpkgs__.lib;
+  head = lib.strings.toUpper (builtins.substring 0 1 str);
+  tail = builtins.concatStringsSep "" (
+    builtins.tail (lib.stringToCharacters str)
+  );
+in
+head + tail
