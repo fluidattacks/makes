0e7c91dea188116d652fb207de28d25a2a1dd626
Kevin Amado <kamadorueda@gmail.com>
2021-09-03 09:51:48 -0500

    feat(build): #494 npm mirror
    - Create an npm mirror based on a symlink
      farm
    - It works now!!
    - Pending to polish some details

---
+++ src/args/make-node-js-npm-environment/default.nix
@@ -0,0 +1,84 @@
+{ __nixpkgs__
+, fromJsonFile
+, toFileJson
+, makeDerivation
+, makeNodeJsVersion
+, ...
+}:
+{ name
+, nodeJsVersion
+, packageJson
+, packageLockJson
+, ...
+}:
+let
+  nodeJs = makeNodeJsVersion nodeJsVersion;
+  packageLock = fromJsonFile packageLockJson;
+
+  collectDependencies = deps:
+    builtins.foldl'
+      (all: name:
+        let
+          dep = depAttrs // rec {
+            inherit name;
+            resolved = __nixpkgs__.fetchurl {
+              hash = depAttrs.integrity;
+              url = depAttrs.resolved;
+            };
+            resolvedName = builtins.baseNameOf resolved;
+          };
+          depAttrs = deps.${name};
+          depsOfdep =
+            if builtins.hasAttr "dependencies" dep
+            then collectDependencies dep.dependencies
+            else [ ];
+        in
+        all ++ depsOfdep ++ [ dep ])
+      [ ]
+      (builtins.attrNames deps);
+
+  dependenciesFlat = collectDependencies packageLock.dependencies;
+  dependenciesGrouped = __nixpkgs__.lib.lists.groupBy
+    (dep: dep.name)
+    (dependenciesFlat);
+
+  registryIndexes = builtins.map
+    (name: {
+      inherit name;
+      path = toFileJson "index.json" {
+        inherit name;
+        versions = builtins.listToAttrs
+          (builtins.map
+            (versionAttrs: {
+              name = versionAttrs.version;
+              value = versionAttrs // {
+                dist = {
+                  integrity = versionAttrs.integrity;
+                  tarball = versionAttrs.resolvedName;
+                };
+              };
+            })
+            (dependenciesGrouped.${name}));
+      };
+    })
+    (builtins.attrNames dependenciesGrouped);
+
+  registryTarballs = builtins.map
+    (dep: { name = dep.resolvedName; path = dep.resolved; })
+    (dependenciesFlat);
+
+  registry = __nixpkgs__.linkFarm "registry"
+    (registryIndexes ++ registryTarballs);
+in
+makeDerivation {
+  builder = ./builder.sh;
+  env = {
+    envRegistry = registry;
+    envPackageJson = packageJson;
+    envPackageLockJson = packageLockJson;
+  };
+  name = "make-node-js-environment-for-${name}";
+  searchPaths = {
+    bin = [ __nixpkgs__.python39 nodeJs ];
+  };
+}
