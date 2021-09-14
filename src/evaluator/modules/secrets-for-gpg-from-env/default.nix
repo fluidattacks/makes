f5ae605781c67a588c2c0a73e81840270e42fa33
Kevin Amado <kamadorueda@gmail.com>
2021-09-14 11:36:56 -0500

    feat(build): #528 secret for gpg from env
    - Add a module that acts as interface to the corresponding
      argument

---
+++ src/evaluator/modules/secrets-for-gpg-from-env/default.nix
@@ -0,0 +1,31 @@
+{ __toModuleOutputs__
+, makeSecretForGpgFromEnv
+, ...
+}:
+{ config
+, lib
+, ...
+}:
+let
+  makeSecretForGpgFromEnvOutput = name: asciiArmorBlocks:
+    {
+      name = "/secretsForGpgFromEnv/${name}";
+      value = makeSecretForGpgFromEnv {
+        inherit name;
+        inherit asciiArmorBlocks;
+      };
+    };
+in
+{
+  options = {
+    secretsForGpgFromEnv = lib.mkOption {
+      default = { };
+      type = lib.types.attrsOf (lib.types.listOf lib.types.str);
+    };
+  };
+  config = {
+    outputs = __toModuleOutputs__
+      makeSecretForGpgFromEnvOutput
+      config.secretsForGpgFromEnv;
+  };
+}
