8462088965f1c21aa08b83dd7d9481fcb45c5ff6
Kevin Amado <kamadorueda@gmail.com>
2021-09-17 13:55:34 -0500

    refac(build): #546 simplify scope and code
    - Refactor code so we know what's flake specific
    - Limit scope of this experimental feature to linux x86-64

--- flake.nix
+++ flake.nix
@@ -2,22 +2,42 @@
   description = "A DevSecOps framework powered by Nix!";
 
   inputs = {
-    flakeUtils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs";
   };
 
-  outputs = { flakeUtils, ... }:
-    (flakeUtils.lib.eachDefaultSystem
-      (system:
-        let makes = import ./default.nix { inherit system; };
+  outputs = { nixpkgs, ... }:
+    let makes = import ./default.nix { system = "x86_64-linux"; };
+    in
+    {
+      defaultPackage.x86_64-linux = makes;
+
+      defaultApp.x86_64-linux.program = "${makes}/bin/m";
+      defaultApp.x86_64-linux.type = "app";
+
+      lib.flakes.evaluate =
+        { inputs
+        , system
+        }:
+        let
+          evaluated = import ./src/evaluator/default.nix {
+            # TODO: Deprecate this
+            makesExecutionId = "123";
+            makesSrc = inputs.makes.outPath;
+            projectSrc = inputs.self.sourceInfo.outPath;
+            # TODO: Make it mutable
+            projectSrcMutable = inputs.self.sourceInfo.outPath;
+            inherit system;
+          };
+          evaluatedOutputs = nixpkgs.lib.mapAttrs'
+            (output: value: {
+              name = "makes:config:outputs:${output}";
+              inherit value;
+            })
+            evaluated.config.outputs;
         in
-        {
-          defaultPackage = makes;
-          defaultApp.program = "${makes}/bin/m";
-          defaultApp.type = "app";
-        }))
-    // {
-      lib = {
-        fromFlake = import ./src/evaluator/from-flake.nix;
-      };
+        evaluatedOutputs // {
+          "makes:config:attrs" = evaluated.config.attrs;
+          "makes:config:cacheAsJson" = evaluated.config.cacheAsJson;
+        };
     };
 }
