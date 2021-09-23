331a7ffd071e0be8d300e23c6380671ee881e516
Kevin Amado <kamadorueda@gmail.com>
2021-09-23 14:13:52 -0500

    feat(build): #614 compute batch module
    - Add a module to parametrize computing jobs
      on batch

--- src/args/compute-on-aws-batch/entrypoint.sh
+++ src/args/compute-on-aws-batch/entrypoint.sh
@@ -5,43 +5,43 @@ function subst_env_vars {
 }
 
 function main {
-  local job_attempts="__argJobAttempts__"
-  local job_attempt_duration_seconds="__argJobAttemptDurationSeconds__"
-  local job_container_overrides
-  local job_command="__argJobCommand__"
-  local job_definition="__argJobDefinition__"
-  local job_manifest="__argJobManifest__"
-  local job_name="__argJobName__"
-  local job_queue="__argJobQueue__"
+  local attempts="__argAttempts__"
+  local attempt_duration_seconds="__argAttemptDurationSeconds__"
+  local container_overrides
+  local command="__argCommand__"
+  local definition="__argDefinition__"
+  local manifest="__argManifest__"
+  local name="__argName__"
+  local queue="__argQueue__"
 
   info Sending job \
-    && job_command="$(subst_env_vars "${job_command}")" \
-    && job_command="$(
+    && command="$(subst_env_vars "${command}")" \
+    && command="$(
       jq -enr \
-        --argjson job_command "${job_command}" \
+        --argjson command "${command}" \
         --args \
-        '($job_command + $ARGS.positional)' \
+        '($command + $ARGS.positional)' \
         -- \
         "${@}"
     )" \
-    && job_manifest="$(subst_env_vars "${job_manifest}")" \
-    && job_container_overrides="$(
+    && manifest="$(subst_env_vars "${manifest}")" \
+    && container_overrides="$(
       jq -enr \
-        --argjson job_manifest "${job_manifest}" \
-        --argjson job_command "${job_command}" \
+        --argjson manifest "${manifest}" \
+        --argjson command "${command}" \
         --args \
-        '$job_manifest * {
-          command: $job_command
+        '$manifest * {
+          command: $command
         }'
     )" \
     && aws batch submit-job \
-      --container-overrides "${job_container_overrides}" \
-      --job-name "${job_name}" \
-      --job-queue "${job_queue}" \
-      --job-definition "${job_definition}" \
-      --retry-strategy "attempts=${job_attempts}" \
-      --timeout "attemptDurationSeconds=${job_attempt_duration_seconds}" \
-    && info Job "${job_name}" has been successfully sentkipped sending it
+      --container-overrides "${container_overrides}" \
+      --job-name "${name}" \
+      --job-queue "${queue}" \
+      --job-definition "${definition}" \
+      --retry-strategy "attempts=${attempts}" \
+      --timeout "attemptDurationSeconds=${attempt_duration_seconds}" \
+    && info Job "${name}" has been successfully sent
 }
 
 main "${@}"
