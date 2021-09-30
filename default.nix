6b18adcda6119f3d7b4b2b9ca6af400c88c2c83e
Kevin Amado <kamadorueda@gmail.com>
2021-09-30 11:08:09 -0500

    feat(build): #548 install bash comp
    - Install bash completions together with the main
      binary

--- default.nix
+++ default.nix
@@ -4,33 +4,49 @@
 
 with import ./src/args/agnostic.nix { inherit system; };
 
-makeScript {
-  aliases = [
-    "m-v21.10"
-    "makes"
-    "makes-v21.10"
-  ];
-  replace = {
-    __argMakesSrc__ = ./.;
-    __argNixStable__ = __nixpkgs__.nixStable;
-    __argNixUnstable__ = __nixpkgs__.nixUnstable;
-  };
-  entrypoint = ''
-    __MAKES_REGISTRY__=__argMakesSrc__/src/cli/main/registry.json \
-    __MAKES_SRC__=__argMakesSrc__ \
-    __NIX_STABLE__=__argNixStable__ \
-    __NIX_UNSTABLE__=__argNixUnstable__ \
-    python __argMakesSrc__/src/cli/main/__main__.py "$@"
-  '';
-  searchPaths = {
-    bin = [
-      __nixpkgs__.cachix
-      __nixpkgs__.git
-      __nixpkgs__.gnutar
-      __nixpkgs__.gzip
-      __nixpkgs__.nixStable
-      __nixpkgs__.python38
+let
+  bin = makeScript {
+    aliases = [
+      "m-v21.10"
+      "makes"
+      "makes-v21.10"
     ];
+    replace = {
+      __argMakesSrc__ = ./.;
+      __argNixStable__ = __nixpkgs__.nixStable;
+      __argNixUnstable__ = __nixpkgs__.nixUnstable;
+    };
+    entrypoint = ''
+      __MAKES_REGISTRY__=__argMakesSrc__/src/cli/main/registry.json \
+      __MAKES_SRC__=__argMakesSrc__ \
+      __NIX_STABLE__=__argNixStable__ \
+      __NIX_UNSTABLE__=__argNixUnstable__ \
+      python __argMakesSrc__/src/cli/main/__main__.py "$@"
+    '';
+    searchPaths = {
+      bin = [
+        __nixpkgs__.cachix
+        __nixpkgs__.git
+        __nixpkgs__.gnutar
+        __nixpkgs__.gzip
+        __nixpkgs__.nixStable
+        __nixpkgs__.python38
+      ];
+    };
+    name = "m";
   };
+  compBash = makeScript {
+    replace = {
+      __argCompBash__ = ./src/comp/bash.sh;
+    };
+    entrypoint = "cat __argCompBash__";
+    name = "m-comp-bash";
+  };
+in
+__nixpkgs__.symlinkJoin {
   name = "m";
+  paths = [
+    bin
+    compBash
+  ];
 }
