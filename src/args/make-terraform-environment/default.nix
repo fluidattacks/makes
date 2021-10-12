281d83b9e7eab98561d154cbda8c5e28d5364c30
Daniel Salazar <dsalazar@fluidattacks.com>
2021-10-11 21:24:42 -0500

    feat(cross): #737 new terraform version
    - Support latest terraform version
    from nixpkgs (1.0)
    - Update tests to use latest version
    - Update documentation
    - Drop 0.16 as it does not exist

--- src/args/make-terraform-environment/default.nix
+++ src/args/make-terraform-environment/default.nix
@@ -7,7 +7,7 @@ let
   terraform = {
     "0.14" = __nixpkgs__.terraform_0_14;
     "0.15" = __nixpkgs__.terraform_0_15;
-    "0.16" = __nixpkgs__.terraform_0_16;
+    "1.0" = __nixpkgs__.terraform_1_0;
   }.${version};
 in
 makeSearchPaths {
