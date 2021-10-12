6ea55c631f336e691ce8f68a23d63ed0c5e72e54
David Arnold <david.arnold@iohk.io>
2021-10-12 15:51:35 -0500

    feat(build): #615 nomad deploy ns module
    - Generate deploy tasks for an entire deploy job tree.

--- src/evaluator/modules/deploy-nomad/default.nix
+++ src/evaluator/modules/deploy-nomad/default.nix
@@ -9,7 +9,7 @@
 }:
 let
   makeOutput = name: { setup, src, namespace, version }: {
-    name = "/deployNomad/${name}";
+    name = "/deployNomad/${namespace}/${name}";
     value = deployNomad {
       inherit setup;
       inherit name;
@@ -18,36 +18,64 @@ let
       inherit version;
     };
   };
+  setupOpt = lib.mkOption {
+    default = [ ];
+    type = lib.types.listOf lib.types.package;
+  };
+  versionOpt = lib.mkOption {
+    type = lib.types.enum [
+      "1.0"
+      "1.1"
+    ];
+  };
 in
 {
   options = {
-    deployNomad = {
-      modules = lib.mkOption {
+    deployNomadJobs = {
+      jobs = lib.mkOption {
         default = { };
         type = lib.types.attrsOf (lib.types.submodule (_: {
           options = {
-            setup = lib.mkOption {
-              default = [ ];
-              type = lib.types.listOf lib.types.package;
-            };
+            setup = setupOpt;
             src = lib.mkOption {
               type = lib.types.str;
             };
             namespace = lib.mkOption {
               type = lib.types.str;
             };
-            version = lib.mkOption {
-              type = lib.types.enum [
-                "1.0"
-                "1.1"
-              ];
+            version = versionOpt;
+          };
+        }));
+      };
+    };
+    deployNomadNamespaces = {
+      namespaces = lib.mkOption {
+        default = { };
+        type = lib.types.attrsOf (lib.types.submodule (_: {
+          options = {
+            setup = setupOpt;
+            jobs = lib.mkOption {
+              type = lib.types.attrsOf lib.types.str;
             };
+            version = versionOpt;
           };
         }));
       };
     };
   };
   config = {
-    outputs = __toModuleOutputs__ makeOutput config.deployNomad.modules;
+    outputs = (
+      lib.mapAttrs
+        (n: v:
+          __toModuleOutputs__ makeOutput (v.jobs // {
+            setup = v.setup;
+            version = v.version;
+            namespacn = n;
+          })
+        )
+        config.deployNomadNamespaces.namespaces
+    ) // (
+      __toModuleOutputs__ makeOutput config.deployNomadJobs.jobs
+    );
   };
 }
