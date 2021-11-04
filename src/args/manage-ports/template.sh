4d34d0993a23ee16981db35efd44587dcccda71b
Diego Restrepo <restrepomesadiego@gmail.com>
2021-11-04 11:11:06 -0500

    feat(back): #686 add wait port
    - function required to wait until a port is available

--- src/args/manage-ports/template.sh
+++ src/args/manage-ports/template.sh
@@ -6,7 +6,7 @@ function kill_port {
 
   pids="$(mktemp)" \
     && if ! lsof -t "-i:${port}" > "${pids}"; then
-      info Nothing listening on port: "${port}" \
+      info "Nothing listening on port: ${port}" \
         && return 0
     fi \
     && while read -r pid; do
@@ -22,11 +22,46 @@ function kill_port {
     done < "${pids}"
 }
 
-function done_port{
+function done_port {
   local host='localhost'
   local port="${1}"
 
   kill_port "${port}" \
-    && echo "[INFO] Done at ${host}:${port}" \
+    && info "Done at ${host}:${port}" \
     && nc -kl "${host}" "${port}"
+}
+
+function wait_for_tcp {
+  local elapsed='1'
+  local timeout="${1}"
+  local host="${2%:*}"
+  local port="${2#*:}"
+
+  while true; do
+    if timeout 1s nc -z "${host}" "${port}"; then
+      return 0
+    elif test "${elapsed}" -gt "${timeout}"; then
+      error "Timeout while waiting for ${host}:${port} to open" \
+        && return 1
+    else
+      info "Waiting 1 second for ${host}:${port} to open, ${elapsed} seconds in total" \
+        && sleep 1 \
+        && elapsed="$(("${elapsed}" + 1))" \
+        && continue
+    fi
+  done
+}
+
+function wait_port {
+  local pids=()
+  local timeout="${1}"
+
+  for address in "${@:2}"; do
+    { wait_for_tcp "${timeout}" "${address}" & } \
+      && pids+=("${!}")
+  done \
+    && for pid in "${pids[@]}"; do
+      wait "${pid}" \
+        || return 1
+    done
 }
