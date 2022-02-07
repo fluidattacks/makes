68754d5a137b74b5f52e06b3eb006c67267e8fe5
John Perez <mrjohnjairo10@gmail.com>
2022-02-07 15:18:43 -0500

    feat(back): #787 support nodejs 17
    - Add nodejs-17_x to makeNodeJsVersion

--- src/args/make-node-js-version/default.nix
+++ src/args/make-node-js-version/default.nix
@@ -10,4 +10,6 @@ else if version == "14"
 then __nixpkgs__.nodejs-14_x
 else if version == "16"
 then __nixpkgs__.nodejs-16_x
+else if version == "17"
+then __nixpkgs__.nodejs-17_x
 else abort "Supported node versions are: 10, 12, 14 and 16"
