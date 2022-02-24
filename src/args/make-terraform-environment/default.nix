9af6fd466e0e50d937c883fd216b98fe9d7527fa
Kevin Amado <kamadorueda@gmail.com>
2022-02-24 17:53:55 -0500

    refac(back): #736 update terraform reference

--- src/args/make-terraform-environment/default.nix
+++ src/args/make-terraform-environment/default.nix
@@ -7,7 +7,7 @@ let
   terraform = {
     "0.14" = __nixpkgs__.terraform_0_14;
     "0.15" = __nixpkgs__.terraform_0_15;
-    "1.0" = __nixpkgs__.terraform_1_0;
+    "1.0" = __nixpkgs__.terraform_1;
   }.${version};
 in
 makeSearchPaths {
