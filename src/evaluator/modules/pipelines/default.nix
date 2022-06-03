77c55f24d48e67c9b5a656900bf4f0d212d6f789
John Perez <mrjohnjairo10@gmail.com>
2022-06-03 14:15:48 -0500

    refac(cross): #0 makes release
    - makes 22.06 is onw the stable version
    - Makes 22.07 is now the unstable version

--- src/evaluator/modules/pipelines/default.nix
+++ src/evaluator/modules/pipelines/default.nix
@@ -92,7 +92,7 @@
     name = toJobName output args;
     value = attrsMerge [
       {
-        image = "ghcr.io/fluidattacks/makes:22.06";
+        image = "ghcr.io/fluidattacks/makes:22.07";
         interruptible = true;
         needs = [];
         script =
