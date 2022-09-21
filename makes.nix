fe78286a67116a5be404ff6062a95b379d9ca8a9
Kevin Amado <kamadorueda@gmail.com>
2022-09-21 09:18:47 -0600

    fix(cross): #0 versioning scheme
    - In september 2022 (22.09),
      the 22.09 tag, release and container,
      should be the stable version of Makes,
      and the 22.10 tag, release and container
      should be the unstable version.
    
      All development that happens during september 2022,
      should go to the 22.10 tag, release and container,
      because the docs promise that:
      "the current month release is frozen",
      so we shouldn't be modifying 22.09.

--- makes.nix
+++ makes.nix
@@ -27,7 +27,7 @@
         };
         registry = "ghcr.io";
         src = outputs."/container-image";
-        tag = "fluidattacks/makes:22.09";
+        tag = "fluidattacks/makes:22.10";
       };
     };
   };
