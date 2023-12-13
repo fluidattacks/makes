550f8dd239f412bd520e32e892ac8a6058403b84
Robin Quintero <rohaquinlop301@gmail.com>
2023-12-13 16:21:59 -0500

    feat(build): #1212 upgrade makenodejslock parameters
    - `makeNodeJsLock` now receives the directory that contains the
      `package.json` file instead of the `package.json` file itself.
    - The `package-lock.json` file will be generated in the same directory
      that contains the `package.json` file.
    - `makeNodeJsLock` now only receives two parameters: `node_js_version`
      and `package_json_dir`.
    - Removed `package_lock` parameter from `makeNodeJsLock`.
    - Updated `makeNodeJsLock` documentation.
    
    Signed-off-by: Robin Quintero <rohaquinlop301@gmail.com>

--- makes/utils/makeNodeJsLock/entrypoint.sh
+++ makes/utils/makeNodeJsLock/entrypoint.sh
@@ -2,21 +2,19 @@
 
 function main {
   local node_js_version="${1}"
-  local package_json="${2}"
+  local package_json_dir="${2}"
   local npm_install_args=(
     --audit false
     --ignore-scripts true
   )
-  local dir_package_json
 
-  : && dir_package_json=$(dirname "${package_json}") \
-    && case "${node_js_version}" in
+  : && case "${node_js_version}" in
       14) npm=__argNode14__/bin/npm ;;
       16) npm=__argNode16__/bin/npm ;;
       18) npm=__argNode18__/bin/npm ;;
       *) critical NodeJs version not supported: "${node_js_version}" ;;
     esac \
-    && pushd "${dir_package_json}" \
+    && pushd "${package_json_dir}" \
     && "${npm}" install "${npm_install_args[@]}" \
     && popd || return 1
 }
