23c42ed57250f372ba77323599a72df95ab44b6b
Robin Quintero <rohaquinlop301@gmail.com>
2024-01-11 11:17:39 -0500

    feat(back): #1249 deprecate makenodejsversion
    - Deprecate `makeNodeJsVersion` from `makeNodeJsLock`
    
    Signed-off-by: Robin Quintero <rohaquinlop301@gmail.com>

--- makes/utils/makeNodeJsLock/main.nix
+++ makes/utils/makeNodeJsLock/main.nix
@@ -1,6 +1,5 @@
 {
   __nixpkgs__,
-  makeNodeJsVersion,
   makeScript,
   ...
 }:
@@ -8,8 +7,8 @@ makeScript {
   entrypoint = ./entrypoint.sh;
   name = "make-node-js-lock";
   replace = {
-    __argNode18__ = makeNodeJsVersion "18";
-    __argNode20__ = makeNodeJsVersion "20";
-    __argNode21__ = makeNodeJsVersion "21";
+    __argNode18__ = __nixpkgs__.nodejs_18;
+    __argNode20__ = __nixpkgs__.nodejs_20;
+    __argNode21__ = __nixpkgs__.nodejs_21;
   };
 }
