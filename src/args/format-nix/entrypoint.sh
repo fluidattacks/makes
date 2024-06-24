2a728f44c6091dcf0dd7cc8ff5f6f1bc6123ef88
Robin Quintero <rquintero@fluidattacks.com>
2024-06-24 13:31:59 -0500

    feat(back): #1332 add check flag to nixfmt
    - Run check only in CI, locally it will format the code
    
    Signed-off-by: Robin Quintero <rohaquinlop301@gmail.com>

--- src/args/format-nix/entrypoint.sh
+++ src/args/format-nix/entrypoint.sh
@@ -2,11 +2,15 @@
 
 function main {
   source __argTargets__/template local targets
+  local args=()
 
   info Formatting Nix code \
+    && if running_in_ci_cd_provider; then
+      args+=(--check)
+    fi \
     && for target in "${targets[@]}"; do
       info Formatting "${target}" \
-        && if ! nixfmt "${target}"; then
+        && if ! nixfmt "${args[@]}" "${target}"; then
           info Source code was formated, the job will fail \
             && error Failing as promised...
         fi \
